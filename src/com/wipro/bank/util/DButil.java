de18521fad9881e945d6637bf75e01dde36ee877
Rajasabari N <rajasabari296@gmail.com>
2026-02-12 09:48:51 +0530

    Add files via upload

---
+++ src/com/wipro/bank/util/DButil.java
@@ -0,0 +1,24 @@
+package com.wipro.bank.util;
+ import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class DButil {
+		
+	public static Connection getDBConnection() {
+		try {
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		String url = "jdbc:oracle:thin:@localhost:1521:XE";
+		String user="system";
+		String pass="sabari140";
+		Connection connection=DriverManager.getConnection(url,user,pass);
+		return connection;    
+		
+	  
+}
+		catch(ClassNotFoundException | SQLException e) {  
+			e.printStackTrace(); 
+			return null;
+}
+	
+}
+}
